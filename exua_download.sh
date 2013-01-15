30a5db83e20be6879f585f3278139a92cacc5607
Roman Rader <antigluk@gmail.com>
2013-01-15 12:17:02 +0200

    fixed file

--- exua_download.sh
+++ exua_download.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-echo "Enter ex.ua article id:"
-read num
+if [ "z$1" = "z" ]; then
+	echo "Enter ex.ua article id:"
+	read num
+else
+	num=$1
+fi
+
+echo $num
+
 URLS="$num.urls"
 curl http://www.ex.ua/filelist/$num.urls > $URLS
-for u in `cat ~/1.txt`; do
+for u in `cat $URLS`; do
 	wget -nc --content-disposition -c $u
 done
